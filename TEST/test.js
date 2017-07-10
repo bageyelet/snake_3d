d968d3e03451ade3f001260aa9e835161efa5bc4
Luca Borzacchiello <lucaborza@gmail.com>
2017-07-10 19:46:24 +0200

    snake move (to be completed) + refactoring snake (still a lot of work to do)

--- TEST/test.js
+++ TEST/test.js
@@ -1,49 +1,65 @@
-function Node(data) {
-    this.data = data;
-    this.next = null;
-    this.prev = null;
+var speed = 5; var max_curr = 200;
+
+function linear_interpolation(v1, min1, max1, min2, max2) {
+    var v2;
+    v2 = v1 * ((max2-min2) / (max1 - min1));
+    return v2;
 }
 
-function List() {
-    this._length = 0;
-    this.head = null;
-    this._nextPointer = this.head;
-    this.add = function(value) {
-        var node = new Node(value);
-        var tmp = this.head;
-        this.head = node;
-        node.next = tmp;
-        node.prev = null;
-        if (tmp != null)
-            tmp.prev = node;
-        this._length++;
-        return node;
+function* genForward() {
+    var inc = linear_interpolation(speed, 0, max_curr, 0, 1);
+    var tot=0;
+    for (var i=0; i<max_curr/speed; i++) {
+        tot+=inc;
+        yield [tot, 0];
     }
-    this.next = function() {
-        if (this._nextPointer == null)
-            this._nextPointer = this.head;
-        else
-            this._nextPointer = this._nextPointer.next;
-        return this._nextPointer;
+    yield [1, 0];
+}
+var forwardArray = [];
+
+var delta = 0.2;
+function* genForwardRotation() {
+    var inc_x = linear_interpolation(speed, 0, max_curr, 0, -1+delta);
+    var inc_y = linear_interpolation(speed, 0, max_curr, 0, -delta);
+    var inc_angle = linear_interpolation(speed, 0, max_curr, 0, 45);
+    var tot = [0, 0]; var tot_angle = 0;
+    for (var i=0; i<max_curr/speed; i++) {
+        tot[0] += inc_x;
+        tot[1] += inc_y;
+        tot_angle += inc_angle;
+        yield [[tot[0], tot[1]], tot_angle];
     }
-    this.print = function() {
-        var p = this.head;
-        while (p!=null) {
-            console.log(p.data);
-            p=p.next;
-        }
+    yield [[-1+delta, -delta], 45];
+}
+var forwardRotationArray = [];
+
+function* genStraigthen() {
+    var inc_x = linear_interpolation(speed, 0, max_curr, 0, 1-delta);
+    var inc_y = linear_interpolation(speed, 0, max_curr, 0, delta);
+    var inc_angle = linear_interpolation(speed, 0, max_curr, 0, 45);
+    var tot = [0, 0]; var tot_angle = 0;
+    for (var i=0; i<max_curr/speed; i++) {
+        tot[0] += inc_x;
+        tot[1] += inc_y;
+        tot_angle += inc_angle;
+        yield [[tot[0], tot[1]], tot_angle];
+    }
+    yield [[1-delta, delta], 45];
+}
+var straigthenArray = [];
+
+var gFor; var gForRot;
+function initializePositionUpdater() {
+    gFor = genForward();
+    gForRot = genForwardRotation();
+    gStr = genStraigthen();
+
+    for (var i=0; i<max_curr + 1; i+=speed) {
+        forwardRotationArray[i] = gForRot.next().value;
+        forwardArray[i] = gFor.next().value;
+        straigthenArray[i] = gStr.next().value;
     }
 }
-var snake = new List();
-snake.add({type:1, pos:[1, 1], angle:0});
-snake.add({type:2, pos:[1, 2], angle:0});
-snake.add({type:3, pos:[1, 3], angle:0});
 
-//console.log(snake);
-
-var el = snake.next();
-while (el != null) {
-    console.log("node", el.data);
-    console.log("prev", el.prev != null? el.prev.data : null);
-    el = snake.next();
-}
+initializePositionUpdater();
+console.log(forwardRotationArray);
